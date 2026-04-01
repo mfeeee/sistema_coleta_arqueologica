d7d37f2563c269323a04e0059c34851c9aaeeea1
mafe <mfernandarocosta@gmail.com>
2026-03-31 21:01:27 -0300

    :lipstick: feat: Definição da paleta de cores do sistema
    Criação do arquivo app_colors.dart contendo as constantes de cores hexadecimais mapeadas do protótipo no Figma para os modos claro e escuro

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Modo Claro 
+  static const Color primaryLight = Color(0xFF493627);
+  static const Color navBarUnfocusedLight = Color(0xFF94A3B8);
+  static const Color backgroundLight = Color(0x1A493627); 
+  static const Color surfaceLight = Color(0xFFFFFFFF); 
+  static const Color surfaceLightSecondary = Color(0x33493627); 
+  static const Color textPrimaryLight = Color(0xFF292524);
+  static const Color textSecondaryLight = Color(0xFF57534E);
+
+  // Modo Escuro
+  static const Color primaryDark = Color(0xFF8B4513);
+  static const Color backgroundDark = Color(0xFF12100E);
+  static const Color surfaceDark = Color(0xFF1C1917);
+  static const Color textPrimaryDark = Color(0xFFFFFFFF);
+  static const Color textSecondaryDark = Color(0xFF94A3B8);
+  static const Color navBarUnfocusedDark = Color(0xFF52525B);
+
+  // Cores de status
+  static const Color error = Color(0xFFC2410C);
+  static const Color warning = Color(0xFFCA8A04);
+  static const Color success = Color(0xFF16A34A);
+  static const Color info = Color(0x66493627);
+}

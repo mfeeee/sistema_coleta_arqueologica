c831a245c2f956f4cdccde8c42c42e78f54e604d
mafe <mfernandarocosta@gmail.com>
2026-04-04 19:38:46 -0300

    :lipstick: fix: Corrige a cor de fundo do modo claro

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -4,7 +4,7 @@ class AppColors {
   // Modo Claro 
   static const Color primaryLight = Color(0xFF493627);
   static const Color navBarUnfocusedLight = Color(0xFF94A3B8);
-  static const Color backgroundLight = Color(0x1A493627); 
+  static const Color backgroundLight = Color(0xFFF7F7F6); 
   static const Color surfaceLight = Color(0xFFFFFFFF); 
   static const Color surfaceLightSecondary = Color(0x33493627); 
   static const Color textPrimaryLight = Color(0xFF292524);

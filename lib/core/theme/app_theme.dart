5ab3a9a51f83c7461a726825104f88e6a9507d51
mafe <mfernandarocosta@gmail.com>
2026-04-07 22:08:28 -0300

    :gear: fix: Refatora as configurações dos temas claro e escuro seguindo o MaterialDesign3

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -5,26 +5,24 @@ class AppTheme {
   // TEMA CLARO
   static ThemeData get lightTheme {
     return ThemeData(
-      brightness: Brightness.light,
-      primaryColor: AppColors.primaryLight,
+      useMaterial3: true,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: AppColors.primaryLight,
+        brightness: Brightness.light,
+      ),
       scaffoldBackgroundColor: AppColors.backgroundLight,
-      
       appBarTheme: const AppBarTheme(
-        backgroundColor: AppColors.primaryLight,
-        foregroundColor: Colors.white,
+        backgroundColor: AppColors.backgroundLight,
+        foregroundColor: AppColors.textPrimaryLight,
         elevation: 0,
         centerTitle: true,
       ),
-
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ElevatedButton.styleFrom(
-          backgroundColor: AppColors.primaryLight,
-          foregroundColor: Colors.white,
           minimumSize: const Size(double.infinity, 50),
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
         ),
       ),
-
       textTheme: const TextTheme(
         displayLarge: TextStyle(color: AppColors.textPrimaryLight, fontWeight: FontWeight.bold),
         bodyLarge: TextStyle(color: AppColors.textPrimaryLight),
@@ -36,26 +34,24 @@ class AppTheme {
   // TEMA ESCURO
   static ThemeData get darkTheme {
     return ThemeData(
-      brightness: Brightness.dark,
-      primaryColor: AppColors.primaryDark,
+      useMaterial3: true,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: AppColors.primaryDark,
+        brightness: Brightness.dark,
+      ),
       scaffoldBackgroundColor: AppColors.backgroundDark,
-      
       appBarTheme: const AppBarTheme(
         backgroundColor: AppColors.backgroundDark,
         foregroundColor: AppColors.textPrimaryDark,
         elevation: 0,
         centerTitle: true,
       ),
-
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ElevatedButton.styleFrom(
-          backgroundColor: AppColors.primaryDark,
-          foregroundColor: AppColors.textPrimaryDark,
           minimumSize: const Size(double.infinity, 50),
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
         ),
       ),
-
       textTheme: const TextTheme(
         displayLarge: TextStyle(color: AppColors.textPrimaryDark, fontWeight: FontWeight.bold),
         bodyLarge: TextStyle(color: AppColors.textPrimaryDark),

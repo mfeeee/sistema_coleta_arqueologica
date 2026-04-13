29770b4b4557ab1b57cce752473b1d37856ffdd5
mafe <mfernandarocosta@gmail.com>
2026-04-12 23:14:23 -0300

    :sparkles: feat: implementa a estrutura da página de notificações e navegação

--- lib/features/notifications/presentation/pages/notifications_page.dart
+++ lib/features/notifications/presentation/pages/notifications_page.dart
@@ -1,8 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 // import 'package:go_router/go_router.dart';
 
 class NotificationsPage extends StatelessWidget {
   const NotificationsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return Scaffold(
+      backgroundColor: theme.scaffoldBackgroundColor,
+      appBar: AppBar(
+        elevation: 0,
+        titleSpacing: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            if (context.canPop()) {
+              context.pop();
+            } else {
+              context.go('/home');
+            }
+          },
+        ),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(1.0),
+          child: Container(
+            color: theme.colorScheme.primary.withValues(alpha: 0.1),
+            height: 1.0,
+          ),
+        ),
+        title: Text(
+          'Notificações',
+          style: theme.textTheme.displayLarge?.copyWith(
+            fontSize: 20,
+            letterSpacing: -0.5,
+          ),
+        ),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
+        children: <Widget>[const _NotificationCard()],
+      ),
+    );
+  }
+}
+
+class _NotificationCard extends StatelessWidget {
+  const _NotificationCard();
 
   @override
   Widget build(BuildContext context) {

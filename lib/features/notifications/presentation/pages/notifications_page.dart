0fd203e8a11279c8a813ee231c74f571f509f3b9
mafe <mfernandarocosta@gmail.com>
2026-04-12 22:52:56 -0300

    :heavy_plus_sign: feat: adiciona estrutura inicial do widget NotificationsPage

---
+++ lib/features/notifications/presentation/pages/notifications_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+// import 'package:go_router/go_router.dart';
+
+class NotificationsPage extends StatelessWidget {
+  const NotificationsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}

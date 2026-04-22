2431b556e1421c325d732a08ff51ec207fe39c38
mafe <mfernandarocosta@gmail.com>
2026-04-22 17:00:18 -0300

    :lipstick: style(sync): formata linha em branco no geolocator_helper

--- lib/core/utils/geolocator_helper.dart
+++ lib/core/utils/geolocator_helper.dart
@@ -3,7 +3,6 @@
 typedef Coordenada = ({double latitude, double longitude});
 
 class GeolocatorHelper {
-
   Future<Coordenada> obterCoordenadaAtual() async {
     await Future.delayed(const Duration(seconds: 2));
     return (latitude: -8.8475, longitude: -42.5480);

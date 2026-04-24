17235636a8e238bfeaec68c0c69d4fc6773a32cf
mafe <mfernandarocosta@gmail.com>
2026-04-24 20:26:57 -0300

    feat(enum): adiciona enums para PerfilUsuario, ClassificacaoUsuario, StatusCuradoria, AcaoResultanteCuradoria e TipoMidia

---
+++ lib/core/database/converters/enum_converters.dart
@@ -0,0 +1,9 @@
+enum PerfilUsuario { coletor, curador, admin }
+
+enum ClassificacaoUsuario { estudante, professor, arqueologo }
+
+enum StatusCuradoria { pendente, aprovado, rejeitado }
+
+enum AcaoResultanteCuradoria { criarSitio, atualizarSitio, rejeitar }
+
+enum TipoMidia { imagem, video, tese, artigo }

f0da5f4bee55a77e573d2c8c3e2801263077ee7f
mafe <mfernandarocosta@gmail.com>
2026-04-25 21:38:48 -0300

    :sparkles: feat(main): refatora a função principal para dar suporte à inicialização segura do banco de dados

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,36 @@ import 'package:flutter/material.dart';
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 import 'core/database/app_database.dart';
+import 'dart:developer';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'core/services/secure_storage_service.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final database = AppDatabase();
+  const secureStorage = SecureStorageService(FlutterSecureStorage());
 
-  runApp(SistemaColetaApp(database: database));
+  late final AppDatabase db;
+  try {
+    final passphrase = await secureStorage.getOrCreateDbPassphrase();
+    db = await AppDatabase.open(passphrase);
+  } catch (e) {
+    log('Falha crítica ao abrir banco criptografado', error: e, name: 'Main');
+    rethrow;
+  }
+
+  runApp(SistemaColetaApp(database: db, secureStorage: secureStorage));
 }
 
 class SistemaColetaApp extends StatelessWidget {
-  const SistemaColetaApp({super.key, required this.database});
+  const SistemaColetaApp({
+    super.key,
+    required this.database,
+    required this.secureStorage,
+  });
 
   final AppDatabase database;
+  final SecureStorageService secureStorage;
 
   @override
   Widget build(BuildContext context) {

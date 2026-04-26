692e567b4ab67ffffc76888d6d52796f57ed98b9
mafe <mfernandarocosta@gmail.com>
2026-04-26 16:43:57 -0300

    :sparkles: feat(di): adiciona coletaRepository ao AppScope e refatora o método create

--- lib/core/di/app_scope.dart
+++ lib/core/di/app_scope.dart
@@ -2,17 +2,38 @@
 import 'package:flutter/widgets.dart';
 import '../../features/auth/auth_notifier.dart';
 import '../../features/sync/sync_service.dart';
+import '../../features/coleta/data/datasources/bem_material_local_datasource.dart';
+import '../../features/coleta/data/repositories/coleta_repository_impl.dart';
+import '../../core/database/app_database.dart';
 
 class AppScope extends InheritedWidget {
   const AppScope({
     super.key,
     required this.authNotifier,
     required this.syncService,
+    required this.coletaRepository,
     required super.child,
   });
 
   final AuthNotifier authNotifier;
   final SyncService syncService;
+  final ColetaRepositoryImpl coletaRepository;
+
+  factory AppScope.create({
+    required AppDatabase database,
+    required AuthNotifier authNotifier,
+    required SyncService syncService,
+    required Widget child,
+  }) {
+    final datasource = BemMaterialLocalDatasourceImpl(database);
+    final coletaRepository = ColetaRepositoryImpl(datasource);
+    return AppScope(
+      authNotifier: authNotifier,
+      syncService: syncService,
+      coletaRepository: coletaRepository,
+      child: child,
+    );
+  }
 
   static AppScope of(BuildContext context) {
     final scope = context.dependOnInheritedWidgetOfExactType<AppScope>();
@@ -23,5 +44,6 @@ class AppScope extends InheritedWidget {
   @override
   bool updateShouldNotify(AppScope oldWidget) =>
       authNotifier != oldWidget.authNotifier ||
-      syncService != oldWidget.syncService;
+      syncService != oldWidget.syncService ||
+      coletaRepository != oldWidget.coletaRepository;
 }

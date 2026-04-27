8dd466c6ad6cd0d305c9d236498cb15cb885f0f0
mafe <mfernandarocosta@gmail.com>
2026-04-27 09:31:02 -0300

    :sparkles: feat(bens_materiais_table): adiciona novas colunas e índices para natureza, tipo e uf; implementa StringListConverter para artefatos

--- lib/core/database/tables/bens_materiais_table.dart
+++ lib/core/database/tables/bens_materiais_table.dart
@@ -3,6 +3,9 @@ import 'coletas_table.dart';
 
 @TableIndex(name: 'bens_coleta_idx', columns: {#coletaId})
 @TableIndex(name: 'municipio', columns: {#municipio})
+@TableIndex(name: 'bens_natureza_idx', columns: {#natureza})
+@TableIndex(name: 'bens_tipo_idx', columns: {#tipo})
+@TableIndex(name: 'bens_uf_idx', columns: {#uf})
 class BensMateriais extends Table {
   TextColumn get uuid => text()();
 
@@ -15,14 +18,21 @@ class BensMateriais extends Table {
   TextColumn get natureza => text()();
   TextColumn get tipo => text()();
   TextColumn get meiosAcesso => text().named('meios_acesso').nullable()();
+  TextColumn get artefatos => text()
+      .named('artefatos')
+      .map(const StringListConverter())
+      .clientDefault(() => '[]')();
   BoolColumn get publicado => boolean().clientDefault(() => false)();
-  TextColumn get uf => text().withLength(min: 2, max: 2)();
-  TextColumn get municipio => text()();
+  TextColumn get uf => text().withLength(min: 2, max: 2).nullable()();
+  TextColumn get municipio => text().nullable()();
   TextColumn get cep => text().nullable()();
   TextColumn get endereco => text().nullable()();
-  TextColumn get latitude => text().nullable()();
-  TextColumn get longitude => text().nullable()();
+  RealColumn get latitude => real().nullable()();
+  RealColumn get longitude => real().nullable()();
   TextColumn get geojson => text().nullable()();
+  IntColumn get anoRegistro => integer().named('ano_registro').nullable()();
+  TextColumn get descricaoAtualizacao =>
+      text().named('descricao_atualizacao').nullable()();
   DateTimeColumn get criadoEm =>
       dateTime().named('criado_em').clientDefault(() => DateTime.now())();
   DateTimeColumn get atualizadoEm =>

92b48b7cfde40b1cf8726a5eba3d9b8d8f4e01a8
mafe <mfernandarocosta@gmail.com>
2026-05-05 10:48:39 -0300

    :zap: refactor(coleta): remove métodos obsoletos e reorganiza a interface do ColetaRepository

--- lib/features/coleta/domain/repositories/coleta_repository.dart
+++ lib/features/coleta/domain/repositories/coleta_repository.dart
@@ -1,7 +1,10 @@
 import '../entities/coleta_entity.dart';
 
 abstract class ColetaRepository {
-  Future<List<ColetaEntity>> getBemMaterialsCacheOffline();
-
-  Future<void> salvarNovaColeta(ColetaEntity novoBemMaterial);
+  Future<List<ColetaEntity>> getAll();
+  Future<List<ColetaEntity>> getPendentes();
+  Future<ColetaEntity?> getById(String uuid);
+  Future<void> salvar(ColetaEntity coleta);
+  Future<void> atualizarStatus(String uuid, dynamic status, int novaVersao);
+  Future<void> deletar(String uuid);
 }

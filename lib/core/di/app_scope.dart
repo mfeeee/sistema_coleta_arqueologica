1d4a1ce049353e6f10069306e9f8138f78c2b532
mafe <mfernandarocosta@gmail.com>
2026-05-07 11:54:56 -0300

    :sparkles: feat(app_scope): refatora o AppScope para incluir o BemMaterialRepository e atualiza a injeção de dependência

--- lib/core/di/app_scope.dart
+++ lib/core/di/app_scope.dart
@@ -1,10 +1,19 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/widgets.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:sistema_coleta_arqueologica/core/services/media_service.dart';
-import 'package:sistema_coleta_arqueologica/features/sync/domain/sync_notifier.dart';
+import 'package:sistema_coleta_arqueologica/core/services/secure_storage_service.dart';
+import 'package:sistema_coleta_arqueologica/features/bem_material/domain/repositories/bem_material_repository.dart';
+import 'package:sistema_coleta_arqueologica/features/coleta/domain/repositories/coleta_repository.dart';
+
+import '../../features/sync/domain/sync_notifier.dart';
 import '../../features/auth/auth_notifier.dart';
 import '../../features/coleta/data/datasources/coleta_local_datasource.dart';
+import '../../features/bem_material/data/datasources/bem_material_local_datasource.dart';
 import '../../features/coleta/data/repositories/coleta_repository_impl.dart';
+import '../../features/bem_material/data/repositories/bem_material_repository_impl.dart';
+import '../../features/sync/data/sync_api_datasource.dart';
+import '../../features/sync/data/sync_repository.dart';
 import '../../core/database/app_database.dart';
 
 class AppScope extends InheritedWidget {
@@ -13,29 +22,49 @@ class AppScope extends InheritedWidget {
     required this.authNotifier,
     required this.syncNotifier,
     required this.coletaRepository,
+    required this.bemMaterialRepository,
     required this.mediaService,
     required super.child,
   });
 
   final AuthNotifier authNotifier;
   final SyncNotifier syncNotifier;
-  final ColetaRepositoryImpl coletaRepository;
+  final ColetaRepository coletaRepository;
+  final BemMaterialRepository bemMaterialRepository;
   final MediaService mediaService;
 
   factory AppScope.create({
     required AppDatabase database,
+    required SecureStorageService secureStorage,
     required AuthNotifier authNotifier,
-    required SyncNotifier syncNotifier,
+    required Dio dio,
     required Widget child,
   }) {
-    final datasource = ColetaLocalDatasourceImpl(database);
-    final coletaRepository = ColetaRepositoryImpl(datasource);
+    final coletaDatasource = ColetaLocalDatasourceImpl(database);
+    final bemMaterialDatasource = BemMaterialLocalDatasourceImpl(database);
+
+    final coletaRepository = ColetaRepositoryImpl(coletaDatasource);
+    final bemMaterialRepository = BemMaterialRepositoryImpl(
+      bemMaterialDatasource,
+    );
+
+    final syncApiDatasource = SyncApiDatasourceImpl(dio);
+    final syncRepository = SyncRepository(
+      coletaDatasource: coletaDatasource,
+      apiDatasource: syncApiDatasource,
+    );
+    final syncNotifier = SyncNotifier(
+      repository: syncRepository,
+      secureStorage: secureStorage,
+    );
+
     final mediaService = MediaService(ImagePicker());
 
     return AppScope(
       authNotifier: authNotifier,
       syncNotifier: syncNotifier,
       coletaRepository: coletaRepository,
+      bemMaterialRepository: bemMaterialRepository,
       mediaService: mediaService,
       child: child,
     );
@@ -52,5 +81,6 @@ class AppScope extends InheritedWidget {
       authNotifier != oldWidget.authNotifier ||
       syncNotifier != oldWidget.syncNotifier ||
       coletaRepository != oldWidget.coletaRepository ||
+      bemMaterialRepository != oldWidget.bemMaterialRepository ||
       mediaService != oldWidget.mediaService;
 }

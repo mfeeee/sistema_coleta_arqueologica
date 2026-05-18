524bd2f26cac89f6520fbbc7dc259073095d0a4d
mafe <mfernandarocosta@gmail.com>
2026-05-18 00:55:35 -0300

    :sparkles: feat(coleta_repository): adiciona métodos para contar todas as coletas e por status, além de obter coletas recentes

--- lib/features/coleta/domain/repositories/coleta_repository.dart
+++ lib/features/coleta/domain/repositories/coleta_repository.dart
@@ -1,9 +1,13 @@
 import '../entities/coleta_entity.dart';
+import 'package:sistema_coleta_arqueologica/core/database/enums/status_coleta.dart';
 
 abstract class ColetaRepository {
   Future<List<ColetaEntity>> getAll();
   Future<List<ColetaEntity>> getPendentes();
   Future<ColetaEntity?> getById(String uuid);
+  Future<int> contarTodas();
+  Future<int> contarPorStatus(StatusColeta status);
+  Future<List<ColetaEntity>> getRecentes(int limite);
   Future<void> salvar(ColetaEntity coleta);
   Future<void> atualizarStatus(String uuid, dynamic status, int novaVersao);
   Future<void> deletar(String uuid);

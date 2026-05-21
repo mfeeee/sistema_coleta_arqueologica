74a6cebfbf4ac5b68ef8030416af6547396860de
mafe <mfernandarocosta@gmail.com>
2026-05-21 11:55:43 -0300

    :bug: fix(coletas_table): adiciona coluna 'sincronizado' com valor padrão false

--- lib/core/database/tables/coletas_table.dart
+++ lib/core/database/tables/coletas_table.dart
@@ -66,6 +66,8 @@ class Coletas extends Table {
       .map(const StringListConverter())
       .withDefault(const Constant('[]'))();
 
+  BoolColumn get sincronizado => boolean().clientDefault(() => false)();
+
   DateTimeColumn get deletadoEm => dateTime().named('deletado_em').nullable()();
 
   @override

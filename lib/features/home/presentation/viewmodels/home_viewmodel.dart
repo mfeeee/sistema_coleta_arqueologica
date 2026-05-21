181be80e1a2d890fb56dec6fccae8ead438ad451
mafe <mfernandarocosta@gmail.com>
2026-05-20 23:34:55 -0300

    :sparkles: feat(home): adiciona tratamento de erros ao carregar dados no HomeViewModel

--- lib/features/home/presentation/viewmodels/home_viewmodel.dart
+++ lib/features/home/presentation/viewmodels/home_viewmodel.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/foundation.dart';
 import 'package:sistema_coleta_arqueologica/core/database/enums/status_coleta.dart';
 import 'package:sistema_coleta_arqueologica/features/auth/auth_notifier.dart';
@@ -11,6 +13,7 @@ class HomeViewModel {
   final ValueNotifier<int> coletasPendentes = ValueNotifier(0);
   final ValueNotifier<List<ColetaEntity>> coletasRecentes = ValueNotifier([]);
   final ValueNotifier<String> nomeUsuario;
+  final ValueNotifier<String?> erroAtual = ValueNotifier(null);
 
   HomeViewModel({
     required ColetaRepository coletaRepository,
@@ -19,11 +22,22 @@ class HomeViewModel {
        nomeUsuario = ValueNotifier(authNotifier.userName ?? '');
 
   Future<void> carregarDados() async {
-    totalColetas.value = await _coletaRepository.contarTodas();
-    coletasPendentes.value = await _coletaRepository.contarPorStatus(
-      StatusColeta.pendente,
-    );
-    coletasRecentes.value = await _coletaRepository.getRecentes(5);
+    erroAtual.value = null;
+    try {
+      totalColetas.value = await _coletaRepository.contarTodas();
+      coletasPendentes.value = await _coletaRepository.contarPorStatus(
+        StatusColeta.pendente,
+      );
+      coletasRecentes.value = await _coletaRepository.getRecentes(5);
+    } catch (e, st) {
+      log(
+        'Erro ao carregar dados da tela inicial',
+        error: e,
+        stackTrace: st,
+        name: 'HomeViewModel',
+      );
+      erroAtual.value = 'Não foi possível carregar os dados.';
+    }
   }
 
   void dispose() {
@@ -31,5 +45,6 @@ class HomeViewModel {
     coletasPendentes.dispose();
     coletasRecentes.dispose();
     nomeUsuario.dispose();
+    erroAtual.dispose();
   }
 }

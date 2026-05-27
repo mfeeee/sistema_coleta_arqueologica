aee35661bd73616517db7259fe104ea2877de757
mafe <mfernandarocosta@gmail.com>
2026-05-27 14:03:36 -0300

    :sparkles: feat(app_spacing): adiciona constantes de espaçamento padronizadas

---
+++ lib/core/theme/app_spacing.dart
@@ -0,0 +1,7 @@
+abstract final class AppSpacing {
+  static const double containerPadding = 16.0;
+  static const double stackGap = 16.0;
+  static const double sectionGap = 24.0;
+  static const double elementPaddingY = 14.0;
+  static const double elementPaddingX = 16.0;
+}

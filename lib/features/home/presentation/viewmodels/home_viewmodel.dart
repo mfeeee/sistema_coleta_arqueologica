a3e135ebfe25dc757f0ccad31b471f1bc0927610
mafe <mfernandarocosta@gmail.com>
2026-06-02 11:55:55 -0300

    :recycle: refactor(home_viewmodel): substitui SitioMapaEntity por PinoMapa em sitiosNoMapa

--- lib/features/home/presentation/viewmodels/home_viewmodel.dart
+++ lib/features/home/presentation/viewmodels/home_viewmodel.dart
@@ -5,7 +5,7 @@ import 'package:sistema_coleta_arqueologica/core/database/enums/status_coleta.da
 import 'package:sistema_coleta_arqueologica/features/auth/auth_notifier.dart';
 import 'package:sistema_coleta_arqueologica/features/coleta/domain/entities/coleta_entity.dart';
 import 'package:sistema_coleta_arqueologica/features/coleta/domain/repositories/coleta_repository.dart';
-import 'package:sistema_coleta_arqueologica/features/home/domain/entities/sitio_mapa_entity.dart';
+import 'package:sistema_coleta_arqueologica/features/home/domain/entities/pino_mapa.dart';
 
 class HomeViewModel {
   final ColetaRepository _coletaRepository;
@@ -13,7 +13,7 @@ class HomeViewModel {
   final ValueNotifier<int> totalColetas = ValueNotifier(0);
   final ValueNotifier<int> coletasPendentes = ValueNotifier(0);
   final ValueNotifier<List<ColetaEntity>> coletasRecentes = ValueNotifier([]);
-  final ValueNotifier<List<SitioMapaEntity>> sitiosNoMapa = ValueNotifier([]);
+  final ValueNotifier<List<PinoMapa>> sitiosNoMapa = ValueNotifier([]);
   final ValueNotifier<String> nomeUsuario;
   final ValueNotifier<String?> erroAtual = ValueNotifier(null);
 

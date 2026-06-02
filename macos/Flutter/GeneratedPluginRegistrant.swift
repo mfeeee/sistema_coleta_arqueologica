f5c4294881a2698f447a8b537179b6d10eb8987b
mafe <mfernandarocosta@gmail.com>
2026-06-01 23:15:29 -0300

    :sparkles: feat(localization): adiciona suporte à localização para o plugin Gtk e AppLinks nas plataformas Linux, macOS e Windows

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import app_links
 import connectivity_plus
 import file_selector_macos
 import flutter_image_compress_macos
@@ -17,6 +18,7 @@ import shared_preferences_foundation
 import sqlcipher_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))

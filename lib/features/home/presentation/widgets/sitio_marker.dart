e83097b60174acb64d1156a78e250df619af078b
mafe <mfernandarocosta@gmail.com>
2026-06-02 10:14:49 -0300

    :sparkles: feat(widget): adiciona o widget SitioMarker para representação visual de locais

---
+++ lib/features/home/presentation/widgets/sitio_marker.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SitioMarker extends StatelessWidget {
+  const SitioMarker({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return Container(
+      width: 16,
+      height: 16,
+      decoration: BoxDecoration(
+        color: colorScheme.primaryContainer,
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: colorScheme.onPrimary,
+          width: 2.0,
+        ),
+      ),
+    );
+  }
+}

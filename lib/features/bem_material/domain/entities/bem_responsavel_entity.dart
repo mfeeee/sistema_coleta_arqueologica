f1b649d37e00624ead5524832276029f534f0a60
mafe <mfernandarocosta@gmail.com>
2026-06-12 19:03:10 -0300

    :sparkles: feat(core): adicionar entidades e modelos de Usuario e BemResponsavel

---
+++ lib/features/bem_material/domain/entities/bem_responsavel_entity.dart
@@ -0,0 +1,8 @@
+import 'package:sistema_coleta_arqueologica/core/entities/usuario_entity.dart';
+
+class BemResponsavelEntity {
+  final UsuarioEntity usuario;
+  final String papel;
+
+  const BemResponsavelEntity({required this.usuario, required this.papel});
+}
